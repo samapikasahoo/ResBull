0cb9e44561307bb9edb2505ae94109fe02afb153
samapikasahoo <100948175+samapikasahoo@users.noreply.github.com>
2022-11-21 17:46:11 +0100

    Update Home.js

--- resbul/src/components/Home.js
+++ resbul/src/components/Home.js
@@ -1,11 +1,22 @@
 import { Link, Outlet } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<style>
+.responsive 
+  width: 100%;
+  max-width: 400px;
+  height: auto;
+
+
+</style>
+</head>
  
  
 function Home() {
  return (
    <div>
-    <h1 style={{color:" black"}}>templates</h1>
+    <h1 style={{color:" black"}}>Templates</h1>
 
 
 <p style={{color: "black"}}>Use template button below for filling form</p>
@@ -13,7 +24,7 @@ function Home() {
        <Link to="usetemplate">
        <span style={{ marginRight: "20px" }}>
         
-       <Button variant="contained" color="primary">UseTemplate</Button>
+       <Button variant="contained" color="primary"><span>Use Template </span></Button>
   
         
        </span>
@@ -21,6 +32,7 @@ function Home() {
 
    
      <img
+       class="responsive" width="700" height="800"
             src="https://reallygooddesigns.com/wp-content/uploads/2022/03/Free-Multipurpose-Resume-Template-for-InDesign-by-Erick-Ragas.png"
             alt=""
         />
